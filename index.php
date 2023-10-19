d5e23228141b9de5021b55d76fc9e0bf81185703
Miguel Fuenmayor <miguelfuenmayormn@gmail.com>
2023-10-19 15:03:22 -0400

    grid establecido

--- index.php
+++ index.php
@@ -4,26 +4,33 @@
  <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" src="normalize.css"/>
-    <link rel="stylesheet" src="styles1.css"/>
+    <link rel="stylesheet" src="styles/normalize.css"/>
+    <link rel="stylesheet" src="styles/styles.css"/>
     
     <title>Mi Tienda</title>
  </head>
  <body class="body">
+  <div class="grid-container">
      <!--BARRA SUPERIOR: INCLUIRÁ EL LOGO, NOMBRE, LISTA DE OPCIONES, CATEGORIAS, BARRA DE BUSQUEDA, USUARIO Y EL CARRITO-->
-    <header class="barra">
+    <header class="barra grid-item">
       <img src="styles/logo.png" class="barra__logo"/>
       <H1 class="barra__titulo">Mi tienda</H1>
+      <div class="barra__busqueda"></div>
+      <div class="barra__categorias"></div>
+      <div class="barra__opciones"></div>
+      <div class="barra__carrito"></div>
+      <div class="barra__usuario"></div>
     </header>
     
-    <div class="lateral"> <!--LATERAL: tendrá la barra de busqueda avanzada, para buscar por precios y categorias, solos será visible tras la primera busqueda-->
+  <!-- <aside class="lateral grid-item"> <!--LATERAL: tendrá la barra de busqueda avanzada, para buscar por precios y categorias, solos será visible tras la primera busqueda
       LATERAL
-    </div>
-    <div class="contenido"> <!--CONTENIDO: aquí se cargaran todas las vistas, los distintos productos, etc -->
+    </aside> -->
+    <div class="contenido grid-item"> <!--CONTENIDO: aquí se cargaran todas las vistas, los distintos productos, etc -->
       CONTENIDO
     </div>
-    <footer class="pie">
+    <footer class="pie grid-item">
       PIE
     </footer>
+  </div>
  </body>
  </html>
