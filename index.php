1a6c17a380fdf620fecf3f58e8dd8e2f9fea55b6
Miguel Fuenmayor <miguelfuenmayormn@gmail.com>
2023-10-19 11:01:08 -0400

    estilos

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
  <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" src="styles/normalize.css"/>
-    <link rel="stylesheet" src="styles/styles1.css"/>
+    <link rel="stylesheet" src="normalize.css"/>
+    <link rel="stylesheet" src="styles1.css"/>
     
     <title>Mi Tienda</title>
  </head>
