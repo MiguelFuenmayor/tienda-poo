e69a73f9b55f017eaa5a56fd6adf973f6c6857a9
Miguel Fuenmayor <miguelfuenmayormn@gmail.com>
2023-10-19 10:49:39 -0400

    empezando estilos

--- index.php
+++ index.php
@@ -4,17 +4,26 @@
  <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" src="styles/normalize.css"/>
+    <link rel="stylesheet" src="styles/styles1.css"/>
+    
     <title>Mi Tienda</title>
  </head>
- <body>
-    <div> <!--BARRA SUPERIOR: INCLUIRÁ EL LOGO, NOMBRE, LISTA DE OPCIONES, CATEGORIAS, BARRA DE BUSQUEDA, USUARIO Y EL CARRITO-->
-        
+ <body class="body">
+     <!--BARRA SUPERIOR: INCLUIRÁ EL LOGO, NOMBRE, LISTA DE OPCIONES, CATEGORIAS, BARRA DE BUSQUEDA, USUARIO Y EL CARRITO-->
+    <header class="barra">
+      <img src="styles/logo.png" class="barra__logo"/>
+      <H1 class="barra__titulo">Mi tienda</H1>
+    </header>
+    
+    <div class="lateral"> <!--LATERAL: tendrá la barra de busqueda avanzada, para buscar por precios y categorias, solos será visible tras la primera busqueda-->
+      LATERAL
     </div>
-    <div> <!--LATERAL: tendrá la barra de busqueda avanzada, para buscar por precios y categorias, solos será visible tras la primera busqueda-->
-
+    <div class="contenido"> <!--CONTENIDO: aquí se cargaran todas las vistas, los distintos productos, etc -->
+      CONTENIDO
     </div>
-    <div> <!--CONTENIDO: aquí se cargaran todas las vistas, los distintos productos, etc -->
-
-    </div>
+    <footer class="pie">
+      PIE
+    </footer>
  </body>
  </html>
